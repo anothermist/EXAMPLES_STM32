2b248125306fdd972b31d67ef441ed26f71b73e7
anothermist <anothermist@gmail.com>
2022-02-12 20:07:33 +0500

    IR Remote Control

---
+++ IR_REMOTE_NO_TIMER/Debug/Src/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/dwt_stm32_delay.c \
+../Src/main.c \
+../Src/stm32f1xx_hal_msp.c \
+../Src/stm32f1xx_it.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32f1xx.c 
+
+OBJS += \
+./Src/dwt_stm32_delay.o \
+./Src/main.o \
+./Src/stm32f1xx_hal_msp.o \
+./Src/stm32f1xx_it.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32f1xx.o 
+
+C_DEPS += \
+./Src/dwt_stm32_delay.d \
+./Src/main.d \
+./Src/stm32f1xx_hal_msp.d \
+./Src/stm32f1xx_it.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32f1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/dwt_stm32_delay.d ./Src/dwt_stm32_delay.o ./Src/main.d ./Src/main.o ./Src/stm32f1xx_hal_msp.d ./Src/stm32f1xx_hal_msp.o ./Src/stm32f1xx_it.d ./Src/stm32f1xx_it.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o ./Src/system_stm32f1xx.d ./Src/system_stm32f1xx.o
+
+.PHONY: clean-Src
+
